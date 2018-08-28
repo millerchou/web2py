08d714345afedb47740a117ea2d7910ea7348b8a
Zhenren Zhou <2690974765@qq.com>
2018-08-28 17:14:10 +0800

    ds

--- models/global_setting.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from gluon.storage import Storage
-from gluon.custom_import import track_changes
-
-track_changes(True)
-WX = Storage(
-    appid='wx4835e96f55b2a3ec', appsecret='890d0dcec1b3c0df5b94d45c45dcb454', grant_type='authorization_code'
-)
